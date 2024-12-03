e740be0f982222cdea72d705bcaf57934973012d
Navinofc44 <Pahasarasanujja@gmail.com>
2024-12-03 22:57:20 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=vQFCHSQA#qYuOSBrrxNjjgtuNV4mlkeEMAEhUgq_f-_A0mcEBD20",
 };
